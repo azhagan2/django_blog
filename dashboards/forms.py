cbc8db8a4d0efb22bd302345d0bf080871cb58aa
azhagan2 <azhagans128@gmail.com>
2026-04-25 10:44:56 +0530

    Manage users func for managers and editors

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from blogs.models import Category, Blog
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class CategoryForm(forms.ModelForm):
     class Meta:
@@ -11,3 +14,15 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('title', 'category','featured_image', 'short_description','blog_body','is_featured','status')
+
+class addUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')
+
+
+class editUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email','first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')
